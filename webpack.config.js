c9144386b58bd2b6b519160eb8b0313d4da6dc1b
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-10-29 20:59:14 -0300

    transpiling to es5

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 const config = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  }
-}
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        // babel-loader es el que hace la conversion de codigo, pero haciendo uso de los preset que se le seteen
+        // en este caso, estan siendo seteados en el archivo de configuracion .babelrc
+        use: "babel-loader",
+        test: /\.js$/,
+      },
+    ],
+  },
+};
 
 module.exports = config;
