9773c649ca255448516d09d46eff181d47e310fa
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-10-29 21:52:01 -0300

    adding image loader along with url-loader to manage file size

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,6 +1,13 @@
+import big from '../assets/big.jpg';
+import small from '../assets/small.jpg';
 import '../styles/image_viewer.css';
 
 const image = document.createElement('img');
-image.src = 'https://lorempixel.com/400/400';
+image.src = small;
 
 document.body.appendChild(image);
+
+const big_image = document.createElement('img');
+big_image.src = big;
+
+document.body.appendChild(big_image);
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const config = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
+    publicPath: 'build/'
   },
   plugins: [new MiniCssExtractPlugin()],
   module: {
@@ -17,9 +18,21 @@ const config = {
         test: /\.js$/,
       },
       {
-        // cuando hay mas de un loader, se ejecutan respetando el orden y la salida de uno es la entrada del otro
+        // cuando hay mas de un loader, se ejecutan respetando el orden (der a izq) y la salida de uno es la entrada del otro
         use: [MiniCssExtractPlugin.loader, "css-loader"],
         test: /\.css$/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        // image-webpack-loader le hace un resize a la imagen
+        // url-loader se fija el tamaño, si la imagen es menor al limite, la incluye en el bundle, sino la incluye en el directorio de salida
+        use: [
+          {
+            loader: "url-loader",
+            options: { limit: 40000 },
+          },
+          "image-webpack-loader",
+        ],
       },
     ],
   },
