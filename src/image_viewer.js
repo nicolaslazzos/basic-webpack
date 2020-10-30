bee092cc5570f60932d43386bef63e43e4c2d2e1
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-10-29 22:48:36 -0300

    basic code splitting implementation

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,13 +1,15 @@
-import big from '../assets/big.jpg';
-import small from '../assets/small.jpg';
-import '../styles/image_viewer.css';
+import big from "../assets/big.jpg";
+import small from "../assets/small.jpg";
+import "../styles/image_viewer.css";
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+  const image = document.createElement("img");
+  image.src = small;
 
-document.body.appendChild(image);
+  document.body.appendChild(image);
 
-const big_image = document.createElement('img');
-big_image.src = big;
+  const big_image = document.createElement("img");
+  big_image.src = big;
 
-document.body.appendChild(big_image);
+  document.body.appendChild(big_image);
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import sum from "./sum";
-import "./image_viewer"; // este tipo de import ejecuta el codigo
+const button = document.createElement('button');
+button.innerText = 'Click Me!';
 
-const total = sum(10, 5);
+// al agregar import() en alguna parte, webpack al verlo, hace el code splitting para no cargar todos los bundles de una, sino a medida que se necesitan
+button.onclick = () => import('./image_viewer').then(module => module.default());
 
-console.log(total);
+document.body.appendChild(button);
